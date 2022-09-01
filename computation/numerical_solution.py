82f7796ea417a5e513b2254e6dc6d84039d6d5f7
Jonas Kell <jonas-kell@web.de>
2022-09-01 12:19:01 +0200

    feat: calculate energy numerically

--- computation/numerical_solution.py
+++ computation/numerical_solution.py
@@ -1,35 +1,102 @@
 import scipy
 import jVMC
 from hamiltonian import get_hamiltonian
+import numpy as np
 
 
 from lattice_parameter_resolver import resolve_lattice_parameters
 
-lattice_parameters = resolve_lattice_parameters(
-    size=1, shape="cubic", periodic=True, random_swaps=0
-)
-L = lattice_parameters["nr_sites"]
 
-hamiltonian_J_parameter = -1.0
-hamiltonian_h_parameter = -0.7
+def get_index_from_z_eigenstate(eigenstate: list):
+    index = 0
 
-hamiltonian = get_hamiltonian(
-    lattice_parameters=lattice_parameters,
-    hamiltonian_J_parameter=hamiltonian_J_parameter,
-    hamiltonian_h_parameter=hamiltonian_h_parameter,
-)
+    for i, spin in enumerate(eigenstate):
+        index += spin << i
 
-sampler = jVMC.sampler.ExactSampler(lambda a: None, (L,))
-base_states = sampler.basis[0:, 0:2, 0:]
-print(base_states)
-print(base_states.shape)
-
-hamiltonian.get_s_primes(base_states)  # computes them and stores internally
-configurations = hamiltonian.sp
-matrix_elements = hamiltonian.matEl
+    return index
 
 
-print(configurations)
-print(configurations.shape)
-print(matrix_elements)
-print(matrix_elements.shape)
+def assemble_hamiltonian_in_eigenbasis(
+    base_states: list,  # 1 x 2^L x L
+    matrix_elements: list,  # 1 x 2^L x m
+    configurations: list,  # 1 x 2^L x m x L
+):
+    assert len(base_states.shape) == 3
+    assert len(matrix_elements.shape) == 3
+    assert len(configurations.shape) == 4
+
+    L = base_states.shape[2]
+    m = configurations.shape[2]
+    nr_states = 2**L
+
+    assert base_states.shape[0] == 1
+    assert base_states.shape[1] == nr_states
+    assert base_states.shape[2] == L
+
+    assert matrix_elements.shape[0] == 1
+    assert matrix_elements.shape[1] == nr_states
+    assert matrix_elements.shape[2] == m
+
+    assert configurations.shape[0] == 1
+    assert configurations.shape[1] == nr_states
+    assert configurations.shape[2] == m
+    assert configurations.shape[3] == L
+
+    hamiltonian = np.zeros((nr_states, nr_states), dtype=np.complex64)
+
+    for i in range(nr_states):
+        base_state_index = get_index_from_z_eigenstate(base_states[0, i])
+
+        for j in range(m):
+            hamiltonian[
+                base_state_index, get_index_from_z_eigenstate(configurations[0, i, j])
+            ] = matrix_elements[0, i, j]
+
+    return hamiltonian
+
+
+if __name__ == "__main__":
+    lattice_parameters = resolve_lattice_parameters(
+        size=8, shape="linear", periodic=True, random_swaps=0
+    )
+    L = lattice_parameters["nr_sites"]
+
+    hamiltonian_J_parameter = -1.0
+    hamiltonian_h_parameter = -0.7
+
+    hamiltonian = get_hamiltonian(
+        lattice_parameters=lattice_parameters,
+        hamiltonian_J_parameter=hamiltonian_J_parameter,
+        hamiltonian_h_parameter=hamiltonian_h_parameter,
+    )
+
+    sampler = jVMC.sampler.ExactSampler(lambda a: None, (L,))
+    base_states = sampler.basis
+    # print(base_states)                    # all obvious eigenstates of the spin-z-operator
+    # print(base_states.shape)              # 1 x 2^L x L
+
+    hamiltonian.get_s_primes(base_states)  # computes them and stores internally
+    configurations = hamiltonian.sp
+    matrix_elements = hamiltonian.matEl
+    # print(configurations)                 # ALL eigenstate vectors that are a result of applying the Hamiltonian to one base_state
+    # print(configurations.shape)           # 1 x 2^L x m x L (where m depends on the complexity of the Hamiltonian)
+    # print(matrix_elements)                # the correspondung weight-factors to the result of applying the Hamiltonian to one base_state (correspond 1:1 with the "configurations" entries)
+    # print(matrix_elements.shape)          # 1 x 2^L x m
+
+    print(
+        "Start to assemble the final matrix. This Code is very unoptimized, because its entirely written in python..."
+    )
+
+    eigenbasis_matrix = assemble_hamiltonian_in_eigenbasis(
+        base_states, matrix_elements, configurations
+    )
+
+    print(
+        "Matrix is assembled, start eigenvalue calculation... This may take EXTREMELY long"
+    )
+
+    eigenvalue = scipy.sparse.linalg.eigsh(
+        A=eigenbasis_matrix, k=1, return_eigenvectors=False
+    )
+
+    print(f"The Eigenvalue is {eigenvalue}, therefore E/L = {eigenvalue/L}")
