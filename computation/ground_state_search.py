fbd5d2e04fddde43bbacd6d65b8d026e410005bc
Jonas Kell <jonas-kell@web.de>
2022-09-02 21:55:01 +0200

    fix: very small numbers do not cause histogram to fail

--- computation/ground_state_search.py
+++ computation/ground_state_search.py
@@ -238,11 +238,10 @@ def execute_ground_state_search(
         if n % 10 == 0:
             # measure QuantumGeometricTensor/QuantumFisherMatrix
             qgt = np.array(tdvpEquation.get_spectrum())
-
             median = np.median(qgt)
             variance = np.var(qgt)
-            minimum = median - 1.5 * variance
-            maximum = median + 1.5 * variance
+            minimum = median - max(1.5 * variance, 0.5)
+            maximum = median + max(1.5 * variance, 0.5)
 
             # cleanup data, because last number is always massive outlier
             qgt = qgt[(qgt >= minimum) & (qgt <= maximum)]
