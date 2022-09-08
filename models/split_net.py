e4c7d8fd7e8e751ad89c68443dd6d9ae63c7eb37
Jonas Kell <jonas-kell@web.de>
2022-09-08 11:12:12 +0200

    fix: complex conversion justification

--- models/split_net.py
+++ models/split_net.py
@@ -16,7 +16,9 @@ class CombineToComplexModule(nn.Module):
         # The previous layers will need to pick up handling the total scaling and the ratio of real to complex
 
     def __call__(self, x_1, x_2):
-        return jax.lax.complex(x_1, x_2)
+        return jax.lax.complex(
+            x_1, x_2
+        )  # this is sufficient to  get an amplitude/phase network, because the network reults get pushed through an exp() in jVMC internally. to be exact, this is the same thing as jVMC does HERE: https://github.com/markusschmitt/vmc_jax/blob/1eb1aa691aa5e9c0ee112aa5c71e8c2136f17d81/jVMC/vqs.py#L31
 
 
 class CombineToComplexNet(nn.Module):
